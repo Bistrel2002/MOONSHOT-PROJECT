b2169ca839728c05f844f76960716aaf998aa8c8
Bistrel2002 <bistrel.tsangue@algosup.com>
2025-08-06 17:49:45 +0200

    initalising the backend

---
+++ LocIndoor/backend/src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { ENV } from "./config/env.js";
+
+const app = express();
+const PORT = ENV.PORT;
+
+app.listen(PORT, () =>{
+    console.log("Server is running123 on port: ", PORT)
+});
